77360816bee67918dee6aa0ba990063cca90cdcd
develcooking <jpf@purelymail.com>
2026-01-20 14:24:50 +0100

    Fixed issues

--- controllers/createNewUser.php
+++ controllers/createNewUser.php
@@ -1,29 +1,24 @@
 <?php
-
 include $_SERVER['DOCUMENT_ROOT'] . "/database/db.php";
 $errorMessages = [];
 
-if (!isset($_SESSION)) {
-    session_start();
-}
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Register
-    if (isset($_POST['register'])) {
-        $name = $_POST['name'];
-        $vorname = $_POST['vorname'];
+    if (isset($_POST['createAccount'])) {
+        $nachName = $_POST['nachname'];
+        $vorName = $_POST['vorname'];
         $username = $_POST['username'];
         $password = $_POST['password'];
         $email = $_POST['email'];
         $art = $_POST['art'];
         
-        if (empty($name) || empty($vorname) || empty($username) || empty($password) || empty($email) || empty($art)) {
-            $errorMessages = "Bitte füllen Sie alle Felder aus!"; //TODO Error Handling
+        if (empty($nachName) || empty($vorName) || empty($username) || empty($password) || empty($email) || empty($art)) {
+            array_push($errorMessages, "Bitte füllen Sie alle Felder aus!");
         } else {
             $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 
             $stmt = $conn->prepare("INSERT INTO Benutzer (name, vorname, username, password, email, art) VALUES (?, ?, ?, ?, ?, ?)");
-            $stmt->bind_param("ssssss", $name, $vorname, $username, $hashedPassword, $email, $art);
+            $stmt->bind_param("ssssss", $nachName, $vorName, $username, $hashedPassword, $email, $art);
 
             if ($stmt->execute()) {
                 echo "Benutzer erfolgreich registriert!";
@@ -32,18 +27,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
 
             $stmt->close();
+            $conn->close();
         }
     }
 }
-
-    // Logout
-    if (isset($_POST['logout'])) {
-        session_destroy();
-        header("Location: " . $_SERVER['PHP_SELF']); // Reload site if logout
-        exit();
-    }
-
-
 
 // Display error messages
 if (!empty($errorMessages)) {
