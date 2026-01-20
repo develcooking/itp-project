6eb5a3f44e6922954081ce96415cf4f397f11fb9
develcooking <jpf@purelymail.com>
2026-01-20 09:48:55 +0100

    renamed style.css to styles.css

--- views/header.php
+++ views/header.php
@@ -5,9 +5,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="../resources/imgs/icon.png">
+    <link rel="icon" type="image/x-icon" href="/resources/imgs/icon.png">
     <title><?=$site_name?></title>
-    <link rel="stylesheet" type="text/css" href="../resources/css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="/resources/css/styles.css" />
 </head>
 <body>
     <div class="header">
