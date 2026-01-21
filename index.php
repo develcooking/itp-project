881efb1f0918960f2f0652fa7be74128e935fe25
develcooking <jpf@purelymail.com>
2026-01-21 09:35:17 +0100

    fix

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 session_start();
 if (!isset($_SESSION['user'])) {
-  include $_SERVER['DOCUMENT_ROOT'] . "/views/header.php";
-  echo '<div>';
-  echo '<h1 style="text-align: center;">You are not allowed to see this side</h1>';
-  echo '<h1 style="text-align: center;">Please log in</h1>';
-  echo '</div>';
-  die();
+  header('Location: ' . "/views/loginsite.php");
 } else {
     header('Location: ' . "/views/startpage.php");
 }
-
 ?>
--- views/header.php
+++ views/header.php
@@ -6,13 +6,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="/resources/imgs/icon.png">
-    <title><?=$site_name?></title>
+    <title>Ausbildungsportal.net</title>
     <link rel="stylesheet" type="text/css" href="/resources/css/styles.css" />
 </head>
 <body>
     <div class="header">
         <a class="hat" href="../index.php" tabindex="-1">
-            <h2><?=$site_name?></h2>
+            <h2>Ausbildungsportal.net</h2>
         </a>
 
         <div id="headerspacer"></div>
