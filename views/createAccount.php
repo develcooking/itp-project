0b425c522a6b63f7e1129ee4f9aa101a73acce23
develcooking <jpf@purelymail.com>
2026-01-21 11:10:32 +0100

    Added selction for Art

--- views/createAccount.php
+++ views/createAccount.php
@@ -11,6 +11,11 @@ include $homepath . "/views/header.php";
             <input type="text" name="nachname" required placeholder="Nachname">
             <input type="text" name="email" required placeholder="E-Mail address">
             <input type="password" name="password" required placeholder="Password">
+            <select name="art" required>
+                <option value="leher">leher</option>
+                <option value="ausbilder">ausbilder</option>
+                <option value="admin">admin</option>
+            </select>
             <button class="submitbtn" type="submit" name="createAccount">Create Account</button>
         </form>
     <?php else: ?>
