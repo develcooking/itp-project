6aa74e859c4266085270f0faa2dbe3dbd37c1dc8
CodeWithLena <eleni_mavridi@hotmail.com>
2026-01-22 12:08:27 +0100

    grammar mistake

--- views/footer.php
+++ views/footer.php
@@ -7,6 +7,6 @@
       &copy; <?= date("Y"); ?> Ausbildungsportal.net. Alle Rechte vorbehalten.
     </div>
 </footer>
-<script src="/resourses/bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js"></script>
+<script src="/resources/bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js"></script>
 
 
