0a6dbca1e9f6c293e42e4be35b8f1ca31efd6678
develcooking <jpf@purelymail.com>
2026-01-23 12:13:28 +0100

    Fix create Events

--- controllers/createEvent.php
+++ controllers/createEvent.php
@@ -8,17 +8,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $title = $_POST['title'];
         $createdBy = $_SESSION['userId'];
         $modifiedBy = $_SESSION['userId'];
-        $start = $_POST['start'];
-        $end = $_POST['end'];
+        $date1 = $_POST['date1'];
+        $time1 = $_POST['time1'];
+        $date2 = $_POST['date2'];
+        $time2 = $_POST['time2'];
         $description = $_POST['description'];
         $jobId = $_POST['jobId'];
+
+        $startDateTime = $date1 . ' ' . $time1 . ':00'; // Ergebnis: "2023-10-27 14:30:00"
+        $endDateTime   = $date2 . ' ' . $time2 . ':00';
         
-        if (empty($title) || empty($createdBy) || empty($start) || empty($end) || empty($Beschreibung)) {
+        if (empty($title) || empty($createdBy) || empty($startDateTime) || empty($endDateTime) || empty($Beschreibung)) {
             array_push($errorMessages, "Bitte füllen Sie alle Felder aus!");    
         } else {
 
             $stmt = $conn->prepare("INSERT INTO Appointments (title, start, end, description, createdBy, modifiedBy, jobId) VALUES (?, ?, ?, ?, ?, ?, ?)");
-            $stmt->bind_param("ssssiii", $title, $start, $end, $description, $createdBy, $modifiedBy ,$jobId);
+            $stmt->bind_param("ssssiii", $title, $startDateTime, $endDateTime, $description, $createdBy, $modifiedBy ,$jobId);
 
             if ($stmt->execute()) {
                 echo "Termin erfolgreich erstellt!";
