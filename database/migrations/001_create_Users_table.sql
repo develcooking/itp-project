c33679dfc61b4d56ab7f334903353dca489f8e82
gabrielderleth <gabriel.derleth@gmail.com>
2026-01-23 10:55:34 +0100

    small changes in user migration

--- database/migrations/001_create_Users_table.sql
+++ database/migrations/001_create_Users_table.sql
@@ -14,7 +14,7 @@ CREATE TABLE `Users` (
   `activated` tinyint(1) NOT NULL DEFAULT 0,
   `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modifiedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-`createdBy` INT,
+  `createdBy` INT,
   `modifiedBy` INT,
   FOREIGN KEY (`createdBy`) REFERENCES Users(`userId`),
   FOREIGN KEY (`modifiedBy`) REFERENCES Users(`userId`),
