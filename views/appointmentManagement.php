e6ab127f708147219c707f0a83831d479af87c9f
develcooking <jpf@purelymail.com>
2026-01-27 13:58:28 +0100

    Added german Buttons

--- views/appointmentManagement.php
+++ views/appointmentManagement.php
@@ -2,58 +2,64 @@
 include $_SERVER['DOCUMENT_ROOT'] . "/database/db.php";
 include $homepath . "/views/header.php";
 
-?>    
+?>
 
-    <script src='../resources/js/fullCalendar.min.js'></script>
+<script src='../resources/js/fullCalendar.min.js'></script>
 <script src='../resources/js/fullCalendarBootstrapPlugin.js'></script>
-    <style>
-      body {
-        padding: 0;
-        font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-        font-size: 14px;
-      }
+<style>
+  body {
+    padding: 0;
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    font-size: 14px;
+  }
 
-      #calendar {
-        max-width: 1100px;
-        margin: 0 auto;
-      }
-    </style>
-  </head>
+  #calendar {
+    max-width: 1100px;
+    margin: 0 auto;
+  }
+</style>
+</head>
 
-  <body>
+<body>
 
-    <div id='calendar'></div>
+  <div id='calendar'></div>
 
-    <script>
-      document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      var calendarEl = document.getElementById('calendar');
 
-        var calendar = new FullCalendar.Calendar(calendarEl, {
+      var calendar = new FullCalendar.Calendar(calendarEl, {
         themeSystem: 'bootstrap5',
         initialView: 'dayGridMonth',
         locale: 'de',
         firstDay: 1,
 
-          headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
           right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-          },
-          
-          // calling events from getEvents.php
-          events: '../controllers/getEvents.php',
+        },
+        buttonText: {
+          today: 'Heute',
+          month: 'Monat',
+          week: 'Woche',
+          day: 'Tag',
+          list: 'Liste'
+        },
 
-          // Optional: Was passiert beim Klick auf ein Event?
-          eventClick: function(info) {
-            alert('Event: ' + info.event.title);
-            // Hier könnte man z.B. zu einer Bearbeiten-Seite weiterleiten
-            // window.location.href = 'edit.php?id=' + info.event.id;
-          }
-        });
+        // calling events from getEvents.php
+        events: '../controllers/getEvents.php',
 
-        calendar.render();
+
+        eventClick: function(info) {
+          alert('Termin\n Title: ' + info.event.title + '\n Beschreibung: ' + info.event.extendedProps.description + '\n Beginn: ' + info.event.start + '\n Ende: ' + info.event.end);
+        }
       });
-    </script>
 
-<?php
-# include "footer.php" ?>
+      calendar.render();
+    });
+  </script>
+
+  <?php
+   include "footer.php" 
+  ?>
