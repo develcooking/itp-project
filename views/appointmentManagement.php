99b3fd0b885a4e28e4b9aeb14ce0488fda94468b
develcooking <jpf@purelymail.com>
2026-01-29 13:26:10 +0100

    moved css extern and added calendar-container

--- views/appointmentManagement.php
+++ views/appointmentManagement.php
@@ -7,48 +7,12 @@ include $homepath . "/views/header.php";
 <script src='../resources/js/fullCalendar.min.js'></script>
 <script src='../resources/js/fullCalendarBootstrapPlugin.js'></script>
 
-</head>
-
-<body>
+<div class="calendar-container">
 
   <div id='calendar'></div>
+</div>
+<script src="/resources/js/loadFullCalender.js"></script>
 
-  <script>
-    document.addEventListener('DOMContentLoaded', function() {
-      var calendarEl = document.getElementById('calendar');
-
-      var calendar = new FullCalendar.Calendar(calendarEl, {
-        themeSystem: 'bootstrap5',
-        initialView: 'dayGridMonth',
-        locale: 'de',
-        firstDay: 1,
-
-        headerToolbar: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-        },
-        buttonText: {
-          today: 'Heute',
-          month: 'Monat',
-          week: 'Woche',
-          day: 'Tag',
-          list: 'Liste'
-        },
-
-        // calling events from getEvents.php
-        events: '../controllers/getEvents.php',
-
-
-        eventClick: function(info) {
-          alert('Termin\n Title: ' + info.event.title + '\n Beschreibung: ' + info.event.extendedProps.description + '\n Beginn: ' + info.event.start + '\n Ende: ' + info.event.end);
-        }
-      });
-
-      calendar.render();
-    });
-  </script>
-
-  <?php
-   include "footer.php" 
-  ?>
+<?php
+include "footer.php"
+?>
