220d1c62ec421f1adbebc33296118e0a711277ae
AleksSmagin <152731196+AleksSmagin@users.noreply.github.com>
2026-03-17 10:29:00 +0100

    Update forum.php

--- views/forum.php
+++ views/forum.php
@@ -21,7 +21,6 @@ if ($isAdmin) {
 } else {
     $bereiche = $forumModel->getBereiche();
 }
-
 $topics = [];
 $posts = [];
 $currentJobName = "Berufsbereich";
@@ -48,7 +47,7 @@ if ($selectedJobId) {
 if ($selectedTopicId) {
     if ($topicModel->getById($selectedTopicId)) {
         $currentTopicName = $topicModel->getName();
-        $posts = $postModel->getByTopicId($selectedTopicId, $_SESSION['userId']);  
+        $posts = $postModel->getByTopicId($selectedTopicId, $_SESSION['userId']);
         }
 }
 ?>
@@ -164,6 +163,19 @@ if ($selectedTopicId) {
                         </form>
 
                         </div>
+                            </div>
+                        </div>
+
+                    <?php endforeach; ?>
+                    <?php endif; ?>
+                </div>
+
+            <div class="card-footer bg-light d-flex justify-content-end align-items-center gap-2">
+                <button class="btn btn-form-sub" data-bs-toggle="modal" data-bs-target="#createPostModal">
+                    <i class="bi bi-reply me-1"></i> Antworten
+                </button>
+            </div>
+
                 <?php elseif ($selectedJobId): ?>
                     <!-- Threads View (Topics) -->
                     <div class="card-header d-flex justify-content-between align-items-center bg-light">
@@ -213,18 +225,8 @@ if ($selectedTopicId) {
                                     </a>
                                 <?php endforeach; ?>
                             </div>
-                        </div>
-
-                    <?php endforeach; ?>
-                    <?php endif; ?>
-                </div>
-
-            <div class="card-footer bg-light d-flex justify-content-end align-items-center gap-2">
-                <button class="btn btn-form-sub" data-bs-toggle="modal" data-bs-target="#createPostModal">
-                    <i class="bi bi-reply me-1"></i> Antworten
-                </button>
-            </div>
-
+                        <?php endif; ?>
+                    </div>
 
         <?php elseif ($selectedJobId): ?>
             <!-- Threads View (Topics) -->
@@ -303,7 +305,6 @@ if ($selectedTopicId) {
         </div>
     </div>
 </div>
-<script src="/resources/js/postCreateEditor.js"></script>    
 
      <script src="/resources/js/postCreateEditor.js"></script>
 
